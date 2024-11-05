af0b97abd3cf9bbd43c84990f004337716a66b94
huanglang <254453043@qq.com>
2024-11-05 18:18:36 +0800

    前端项目初始化

---
+++ langdada-frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-if="route.path.startsWith('/user')">
+    <router-view></router-view>
+  </template>
+  <template v-else>
+    <BasicLayout />
+  </template>
+</template>
+
+<style>
+#app {
+}
+
+nav {
+  padding: 30px;
+}
+</style>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+</script>
